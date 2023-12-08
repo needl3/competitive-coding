6ffdd1fd93c4066aa13adc10cee5fa83dac50708
needl3 <57314527+needl3@users.noreply.github.com>
2023-12-08 10:06:31 +0545

    day4 done

---
+++ 2023/day4/puzzle2/solution.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// file, _ := os.Open("../input.txt")
+	file, _ := os.Open("../input-submit.txt")
+
+	sum := 0
+	cardMap := map[int]int{}
+	for i := 1; ; i++ {
+		if line, err := Readline(file); err == nil {
+			nMatches := findMatches(line)
+			fmt.Println(i, ": ", nMatches, " matches")
+			if _, ok := cardMap[i]; ok {
+				cardMap[i]++
+			} else {
+				cardMap[i] = 1
+			}
+			for j := i + 1; j <= i+nMatches; j++ {
+				if _, ok := cardMap[j]; ok {
+					cardMap[j] += cardMap[i]
+				} else {
+					cardMap[j] = cardMap[i]
+				}
+			}
+			sum += cardMap[i]
+			fmt.Println("Map: ", cardMap)
+		} else {
+			break
+		}
+	}
+	fmt.Println("Sum: ", sum)
+}
+func findMatches(line string) int {
+	wonMap := map[int]bool{}
+	splitted := strings.Split(line, ": ")
+	info := strings.Split(splitted[1], " | ")
+	won := strings.TrimSpace(info[0])
+	have := strings.TrimSpace(info[1])
+	for _, w := range strings.Split(won, " ") {
+		if n, err := strconv.Atoi(w); err == nil {
+			wonMap[n] = true
+		}
+	}
+	matches := 0
+	for _, h := range strings.Split(have, " ") {
+		if n, err := strconv.Atoi(h); err == nil {
+			if _, ok := wonMap[n]; ok {
+				matches++
+			}
+		}
+	}
+	return matches
+}
+
+func Readline(f *os.File) (string, error) {
+	var line []byte
+	singleFileContent := make([]byte, 1)
+	i := 0
+	for {
+		if read, err := f.Read(singleFileContent); err == nil && read > 0 {
+			if singleFileContent[0] != byte('\n') {
+				line = append(line, singleFileContent...)
+			} else {
+				break
+			}
+		} else {
+			return "", errors.New("eof")
+		}
+		i = i + 1
+	}
+	return string(line), nil
+}
